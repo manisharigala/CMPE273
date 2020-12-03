415d1b6f1d6330d1fa96755c09c1eb7fbcb339ab
manisharigala <arigalamanish@gmail.com>
2020-12-03 13:05:02 -0800

    add node functionality

--- project/client_producer.py
+++ project/client_producer.py
@@ -2,9 +2,13 @@ import zmq
 import time
 import sys
 from itertools import cycle
+import consul
+from bisect import bisect
 
+SERVER_PORT = 0
+producers = {}
 def create_clients(servers):
-    producers = {}
+    global producers 
     context = zmq.Context()
     for server in servers:
         print(f"Creating a server connection to {server}...")
@@ -12,6 +16,31 @@ def create_clients(servers):
         producer_conn.bind(server)
         producers[server] = producer_conn
     return producers
+
+def create_one_client(server):
+    global producers 
+    context = zmq.Context()
+    print(server)
+    print(f"Creating a server connection to {server}...")
+    producer_conn = context.socket(zmq.PUSH)
+    producer_conn.bind(server)
+    producers[server] = producer_conn
+    return producers
+
+# -----------------------------------------------------------------------------------------
+
+def generate_data_round_robin(servers):
+    print("Starting...Round Robin")
+    producers = create_clients(servers)
+    pool = cycle(producers.values())
+    for num in range(10):
+        data = { 'op' : 'PUT','key': f'key-{num}', 'value': f'value-{num}' }
+        print(f"Sending data:{data}")
+        next(pool).send_json(data)
+        time.sleep(1)
+    print("Done")
+
+# ------------------------------------------------------------------------------------------------
 
 def to_int(string):
     res = ""
@@ -24,16 +53,102 @@ def hash_func(x):
     b = 123
     return (a*x + b) % 360 
 
-def generate_data_round_robin(servers):
-    print("Starting...Round Robin")
-    producers = create_clients(servers)
-    pool = cycle(producers.values())
-    for num in range(10):
-        data = { 'key': f'key-{num}', 'value': f'value-{num}' }
-        print(f"Sending data:{data}")
-        next(pool).send_json(data)
+def add_node():
+    
+    global producers
+    context = zmq.Context()
+    consumer = context.socket(zmq.PULL)
+    consumer.bind(f"tcp://127.0.0.1:3000")
+
+    con_servers = list(c.agent.services().keys())
+    servers = ["tcp://"+server for server in con_servers]
+
+    bins_angle_to_name = dict()
+    bin_angle_list = []
+    for s in servers:
+        bins_angle_to_name[hash_func(to_int(s))] = s 
+        bin_angle_list.append(hash_func(to_int(s)))
+    
+    #sort bin_angle_list
+    bin_angle_list.sort()
+    new_node_angle = hash_func(to_int("tcp://127.0.0.1:200{SERVER_PORT}"))
+ 
+    # Finding new Node's position
+    pos = bisect(bin_angle_list,new_node_angle)
+    pos -= 1
+
+    if pos == len(bin_angle_list):
+        #node is first node
+        node = bin_angle_list[0]
+    else :
+        node = bin_angle_list[pos]
+        # node is the pos node
+
+    # Getting old node's data
+    json_string = {'op' : 'GET_ALL' }
+    # print(producers)
+    # print(producers[bins_angle_to_name[node]])
+    # print(node)
+    producers[bins_angle_to_name[node]].send_json(json_string)
+    recv_json = consumer.recv_json()
+    # print(recv_json)
+    recv_json = recv_json['Collection']
+
+    # Adding New Server
+    global SERVER_PORT
+    port = 2000 + SERVER_PORT
+    c.agent.service.register(name=f"127.0.0.1:200{SERVER_PORT}", port = port)
+    new_server = f'tcp://127.0.0.1:200{SERVER_PORT}'
+    SERVER_PORT += 1
+    # print(new_server)
+    producers = create_one_client(new_server)
+    
+
+    # After adding new server
+
+    con_servers = list(c.agent.services().keys())
+    servers = ["tcp://"+server for server in con_servers]
+
+    bins_angle_to_name = dict()
+    bin_angle_list = []
+    for s in servers:
+        bins_angle_to_name[hash_func(to_int(s))] = s 
+        bin_angle_list.append(hash_func(to_int(s)))
+    
+    bin_angle_list.sort()
+    #producers = create_clients(servers)
+    print("Rehashing")
+    print(bin_angle_list)
+    for data in recv_json:
+        hashed_data = hash_func(to_int(data['key']))
+        print("Key Hash value:", hashed_data)
+        # get the consistent hashing node 
+        # send it to the node
+        angle = -1
+        for i in bin_angle_list:
+            if  i >= hashed_data:
+                angle = i
+                break
+        data['op'] = 'PUT'
+        if angle == -1:
+            bin_to_be_hashed_to = bins_angle_to_name[bin_angle_list[0]]
+            print("bin to be hashed to", bin_to_be_hashed_to)
+        else:
+            bin_to_be_hashed_to = bins_angle_to_name[angle]
+            print("bin to be hashed to", bin_to_be_hashed_to)
+        producers[bin_to_be_hashed_to].send_json(data)
         time.sleep(1)
     print("Done")
+
+    #####################
+    # get the position of new node
+    # get all elements from the next server position
+
+
+
+def remove_node(servers, del_node):
+    pass
+
 
 
 def generate_data_consistent_hashing(servers):
@@ -49,7 +164,7 @@ def generate_data_consistent_hashing(servers):
     
     print(bin_angle_list)
     for num in range(10):
-        data = { 'key': f'key-{num}', 'value': f'value-{num}' }
+        data = { 'op':'PUT','key': f'key-{num}', 'value': f'value-{num}' }
         hashed_data = hash_func(to_int(data['key'])) 
         print("Key Hash value:", hashed_data)
         bin_angle_list.sort()
@@ -69,6 +184,7 @@ def generate_data_consistent_hashing(servers):
         time.sleep(1)
     print("Done")
 
+# ----------------------------------------------------------------------------------------------------------
 
 def weight(server, key):
     a = 2347
@@ -92,7 +208,7 @@ def generate_data_hrw_hashing(servers):
     ## TODO
     producers = create_clients(servers)
     for num in range(10):
-        data = { 'key': f'key-{num}', 'value': f'value-{num}' }
+        data = { 'op':'put','key': f'key-{num}', 'value': f'value-{num}' }
         print("Server List:", servers)
         print("Data to be hashed to", data['key'])
         bin_to_be_hashed_to = hrw_bin_to(servers, data['key'])
@@ -101,22 +217,34 @@ def generate_data_hrw_hashing(servers):
         producers[bin_to_be_hashed_to].send_json(data)
         time.sleep(1)
     print("Done")
-    
+
+# -----------------------------------------------------------------------------------------------------------------    
     
 if __name__ == "__main__":
-    servers = []
-    num_server = 1
-    if len(sys.argv) > 1:
-        num_server = int(sys.argv[1])
-        print(f"num_server={num_server}")
-        
-    for each_server in range(num_server):
-        server_port = "200{}".format(each_server)
-        servers.append(f'tcp://127.0.0.1:{server_port}')
-        
-    print("Servers:", servers)
-
-    generate_data_round_robin(servers)
-    generate_data_consistent_hashing(servers)
-    generate_data_hrw_hashing(servers)
+    # servers = []
+    # num_server = 4
+    c = consul.Consul()
     
+    # Initial Service registration
+    for i in range(4):
+        port = 2000 + SERVER_PORT
+        c.agent.service.register(name=f"127.0.0.1:200{SERVER_PORT}", port = port)
+        SERVER_PORT += 1
+
+
+    con_servers = list(c.agent.services().keys())
+    servers = ["tcp://"+server for server in con_servers]
+
+
+    # generate_data_round_robin(servers)
+    generate_data_consistent_hashing(servers)
+    # generate_data_hrw_hashing(servers)
+
+    print("Adding New Node")
+    add_node()
+
+    # con_servers = list(c.agent.services().keys())
+    # servers = ["tcp://"+server for server in con_servers]
+
+    # generate_data_round_robin(servers)
+   
--- project/server_consumer.py
+++ project/server_consumer.py
@@ -1,29 +1,110 @@
 import zmq
 import sys
 from  multiprocessing import Process
+import consul
+import time
+
+
+SERVER_PROCESS_IDS = dict()
+c = consul.Consul()
 
 def server(port):
     context = zmq.Context()
     consumer = context.socket(zmq.PULL)
     consumer.connect(f"tcp://127.0.0.1:{port}")
+    producer = context.socket(zmq.PUSH)
+    producer.connect(f"tcp://127.0.0.1:3000")
     data = dict()
     
     while True:
         raw = consumer.recv_json()
-        key, value = raw['key'], raw['value']
-        print(f"Server_port={port}:key={key},value={value}")
-        # FIXME: Implement to store the key-value data.
-        data[key] = value
+        print(raw)
+        if raw['op'] == 'GET_ALL':
+            print(raw)
+            json_list = []
+            for key in data.keys():
+                json_val = {'key' : key, 'value' : data[key]}
+                json_list.append(json_val)
+            json_string = {'Collection' : json_list}
+            producer.send_json(json_string)
+            print('sent')
+            data = {}
         
+        elif raw['op'] == 'PUT':
+            key, value = raw['key'], raw['value']
+            print(f"Server_port={port}:key={key},value={value}")
+            data[key] = value
+        
+        elif raw['op'] == 'GET':
+            json_string = {'key' : key, 'value' : data[key]}
+            producer.send_json(json_string)
+        
+        else: 
+            pass
+
+def create_servers():
+    con_servers = list(c.agent.services().keys())
+    for s in con_servers:
+        if s not in SERVER_PROCESS_IDS.keys():
+            server_port = c.agent.services()[s]["Port"]
+            print(f"Starting a server at:{server_port}...")
+            process_id = Process(target=server, args=(str(server_port),))
+            SERVER_PROCESS_IDS["127.0.0.1:"+str(server_port)] = process_id
+            process_id.start()
+            time.sleep(1)
+    # print("create stop")
+
+def delete_servers():
+    con_servers = list(c.agent.services().keys())
+    for sp in SERVER_PROCESS_IDS.keys():
+        if sp not in con_servers:
+            process_id = SERVER_PROCESS_IDS[sp]
+            process_id.terminate()
+            SERVER_PROCESS_IDS.pop(sp)
+
         
 if __name__ == "__main__":
-    num_server = 1
-    if len(sys.argv) > 1:
-        num_server = int(sys.argv[1])
-        print(f"num_server={num_server}")
-        
-    for each_server in range(num_server):
-        server_port = "200{}".format(each_server)
-        print(f"Starting a server at:{server_port}...")
-        Process(target=server, args=(server_port,)).start()
     
+    curr_num_servers = len(c.agent.services())
+    create_servers()
+    # print('end main')
+    while True:
+
+        con_num_servers = len(c.agent.services())
+        if curr_num_servers < con_num_servers:
+            # Create Server
+            # servers_to_spawn = con_num_servers - curr_num_servers
+            # print("creating servers")
+            create_servers()
+            curr_num_servers = con_num_servers
+
+        elif curr_num_servers > con_num_servers:
+            delete_servers()
+            curr_num_servers = con_num_servers
+            
+        else:
+            # time.sleep(1)
+             pass
+            # print("Waiting")
+        time.sleep(1)
+
+            # Delete Server
+    # if len(sys.argv) > 1:
+    #     num_server = int(sys.argv[1])
+    #     print(f"num_server={num_server}")
+        
+    # for each_server in range(num_server):
+    #     server_port = "200{}".format(each_server)
+    #     print(f"Starting a server at:{server_port}...")
+    #     Process(target=server, args=(server_port,)).start()
+
+    # for each_server in range(num_server):
+    #     server_port = "200{}".format(each_server)
+    #     print(f"Starting a server at:{server_port}...")
+    #     server_port = int(server_port)
+    #     c.agent.service.register(name=f'127.0.0.1:{server_port}', address=f'127.0.0.1:{server_port}', port=server_port)
+    #     Process(target=server, args=(server_port,)).start()
+
+
+        
+
